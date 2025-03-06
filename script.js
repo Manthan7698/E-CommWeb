137d13e9d7257c88661cacdf6606afed2bc5d56e
HHet-Bhavsar <hhetbhavsar@gmail.com>
2025-03-06 14:29:12 +0530

    scrpit js

--- script.js
+++ script.js
@@ -13,3 +13,16 @@ if(close){
         nav.classList.remove('active');
     });
 }
+
+const container = document.getElementById("container");
+const registerbtn = document.getElementById("register");
+const loginbtn = document.getElementById("login");
+
+registerbtn.addEventListener("click", () => {
+  container.classList.add("active");
+});
+
+loginbtn.addEventListener("click", () => {
+  container.classList.remove("active");
+});
+
