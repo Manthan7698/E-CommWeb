1184725a7b714f96099ccadc22cbf0c842b6c406
Manthan7698 <kukadiyamanthan06@gmail.com>
2025-03-22 14:07:06 +0530

    added about

---
+++ about.php
@@ -0,0 +1,174 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>E-Commerce</title>
+    <link rel="stylesheet" href="style.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" />
+</head>
+
+<body>
+    <section id="header">
+        <a href="#"><img src="img/logo.png" alt="Logo"></a>
+        <div class="search-box">
+            <input type="text" placeholder="Search...">
+            <button name="search-btn" type="submit" title="Search"><i class="fa-solid fa-search"></i></button>
+        </div>
+        <div>
+            <ul id="navbar">
+                <!-- <li id="Search"><input type="text" style="padding-left: 15px;" placeholder="Search"></li> -->
+                <li><a class="active" href="index.php">Home</a></li>
+                <li><a href="shop.php">Shop</a></li>
+                <li><a href="blog.php">Blog</a></li>
+                <li><a href="about.php">About</a></li>
+                <li><a href="contact.php">Contact Us</a></li>
+                <li id="lg-bag">
+                    <a href="bag.php" title="Shopping Bag"><i class="fa-solid fa-bag-shopping"></i></a>
+                    <span id="bag-item-count">0</span>
+                </li>
+                <li><a id="login-btn" href="login.php">Login</a></li>
+                <li><a href="#" id="close" title="Close"><i class="fa-solid fa-xmark"></i></a></li>
+            </ul>
+        </div>
+        <div id="mobile">
+            <a href="bag.html" title="Shopping Bag"><i class="fa-solid fa-bag-shopping"></i></a>
+            <i id="bar" class="fa-solid fa-outdent"></i>
+        </div>
+    </section>
+
+    <section id="s-pg-header" class="about-header">
+        <h2>#KnowUs</h2>
+        <p>Read all case studies about our products!</p>
+    </section>
+
+    <section id="about-head" class="section-p1">
+        <img src="img/about/a6.jpg" alt="">
+        <div>
+            <h2>Who We Are?</h2>
+            <p>An ecommerce website is your digital storefront on the internet. It facilitates the transaction between a
+                buyer and seller. It is the virtual space where you showcase products, and online customers make
+                selections.
+                Your website acts as the product shelves, sales staff, and cash register of your online business
+                channel.Ecommerce (or electronic commerce) is the buying and selling of goods or services on the
+                Internet.
+                It encompasses a wide variety of data, systems and tools for online buyers and sellers, including mobile
+                shopping and online payment encryption.Most businesses with an online presence use an online store
+                and/or
+                platform to conduct ecommerce marketing and sales activities and to oversee logistics and fulfillment.
+            </p>
+            <abbr title="">Create stunning images with as much or as little control as you like thanks to a choice of
+                basic and Creative models.</abbr>
+
+            <br><br>
+
+            <marquee bgcolor="#ccc" loop="-1" scrollamount="5" width="100%">Create stunning images with as much or as little control as you like thanks to a choice of
+                basic and Creative models.</marquee>
+        </div>
+    </section>
+
+    <section id="about-app" class="section-p1">
+        <h1>Download Our <a href="#">App</a></h1>
+        <div class="video">
+            <video autoplay muted loop src="img/about/1.mp4"></video>
+        </div>
+    </section>
+
+    <section id="feature" class="section-p1">
+        <div class="fe-box">
+            <img src="img/features/f1.png" alt="Feature_1">
+            <h6>Free Trails</h6>
+        </div>
+        <div class="fe-box">
+            <img src="img/features/f2.png" alt="Feature_2">
+            <h6>Online Order</h6>
+        </div>
+        <div class="fe-box">
+            <img src="img/features/f3.png" alt="Feature_3">
+            <h6>Save Money</h6>
+        </div>
+        <div class="fe-box">
+            <img src="img/features/f4.png" alt="Feature_4">
+            <h6>Promotion</h6>
+        </div>
+        <div class="fe-box">
+            <img src="img/features/f5.png" alt="Feature_5">
+            <h6>Happy Sells</h6>
+        </div>
+        <div class="fe-box">
+            <img src="img/features/f6.png" alt="Feature_6">
+            <h6>24/7 Support</h6>
+        </div>
+    </section>
+
+    <section id="newsletter" class="section-p1 section-m1">
+        <div class="newstext">
+            <h4>Sign Up For Newsletters</h4>
+            <p>Get E-mail updates about our latest shop and <span>special offers.</span></p>
+        </div>
+        <div class="form">
+            <input type="email" placeholder="Enter Your Email">
+            <button class="normal">Sign Up</button>
+        </div>
+    </section>
+
+    <footer class="section-p1">
+        <div class="col">
+            <img class="logo" src="img/logo.png" alt="" />
+            <h4>Contact</h4>
+            <p>
+                <strong>Address:</strong> 562 Wellington Road, Street 32, San
+                Francisco
+            </p>
+            <p><strong>Phone:</strong> (+91) 9510670132 / (+91) 8866830048</p>
+            <p><strong>Hours:</strong> 10:00 - 18:00, Mon - Sat</p>
+            <div class="follow">
+                <h4>Follow Us</h4>
+                <div class="icons">
+                    <a href="#" title="Facebook"><i class="fa-brands fa-facebook-f"></i></a>
+                    <a href="#" title="Twitter"><i class="fa-brands fa-x-twitter"></i></a>
+                    <a href="#" title="Instagram"><i class="fa-brands fa-instagram"></i></a>
+                    <a href="#" title="Pinterest"><i class="fa-brands fa-pinterest-p"></i></a>
+                    <a href="#" title="YouTube"><i class="fa-brands fa-youtube"></i></a>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <h4>About</h4>
+            <a href="#">About us</a>
+            <a href="#">Delivery Information</a>
+            <a href="#">Privacy Policy</a>
+            <a href="#">Terms & Conditions</a>
+            <a href="#">Contact Us</a>
+        </div>
+
+        <div class="col">
+            <h4>My Account</h4>
+            <a href="#">Sing-in</a>
+            <a href="#">View Cart</a>
+            <a href="#">My Wishlist</a>
+            <a href="#">Track My Order</a>
+            <a href="#">Help</a>
+        </div>
+
+        <div class="col install">
+            <h4>Install App</h4>
+            <p>From App Store or Google Play Store</p>
+            <div class="row">
+                <img src="img/pay/app.jpg" alt="" />
+                <img src="img/pay/play.jpg" alt="" />
+            </div>
+            <p>Secured Payment Gateways</p>
+            <img src="img/pay/pay.png" alt="" />
+        </div>
+        <div class="copyright">
+            <p>© 2024, E-Commerce Website</p>
+        </div>
+    </footer>
+
+    <script src="https://kit.fontawesome.com/0164451027.js" crossorigin="anonymous"></script>
+    <script src="script.js"></script>
+</body>
+
+</html>
