cebb5e8a6e0989911b737dd123b146fe2c0eab49
Manthan7698 <kukadiyamanthan06@gmail.com>
2025-03-25 12:59:56 +0530

    updated script

--- script.js
+++ script.js
@@ -32,25 +32,6 @@ if (loginbtn) {
   });
 }
 
-// Add To Bag Buttons
-
-// const addToBag = document.getElementById("add-to-bag");
-// const spAddToBag = document.getElementById("shop-add-to-bag");
-
-// if (addToBag) {
-//   addToBag.onclick = function () {
-//     var bagCount = document.getElementById("bag-item-count");
-//     bagCount.textContent = parseInt(bagCount.textContent) + 1;
-//   };
-// }
-
-// if (spAddToBag) {
-//   spAddToBag.onclick = function () {
-//     var spBagCount = document.getElementById("shop-bag-item-count");
-//     spBagCount.textContent = parseInt(spBagCount.textContent) + 1;
-//   };
-// }
-
 // Single Product Display
 
 const MainImg = document.getElementById("MainImg");
@@ -78,38 +59,3 @@ function load_cart_item_number() {
     }
   });
 }
-
-// Make sure to call this when page loads
-$(document).ready(function() {
-  // Load cart count immediately when page loads
-  load_cart_item_number();
-
-  // Update cart when adding items
-  $(".addItemBtn").click(function(e) {
-    e.preventDefault();
-    var $form = $(this).closest(".form-submit");
-    var pid = $form.find(".pid").val();
-    var pname = $form.find(".pname").val();
-    var pprice = $form.find(".pprice").val();
-    var pimage = $form.find(".pimage").val();
-    var pcode = $form.find(".pcode").val();
-
-    $.ajax({
-      url: "action.php",
-      method: "post",
-      data: {
-        pid: pid,
-        pname: pname,
-        pprice: pprice,
-        pimage: pimage,
-        pcode: pcode
-      },
-      success: function(response) {
-        $("#message").html(response);
-        window.scrollTo(0,1050);
-        load_cart_item_number(); // Reload cart count after adding item
-      }
-    });
-  });
-});
-
