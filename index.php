1b6bc777e21b7ca64738eebd6c9a2df4353a2580
Manthan7698 <kukadiyamanthan06@gmail.com>
2025-03-25 12:59:22 +0530

    updated index

--- index.php
+++ index.php
@@ -137,7 +137,7 @@
             while ($row = $result->fetch_assoc()):
             ?>
                 <div class="pro">
-                    <img src="<?= $row['product_img'] ?>" alt="">
+                    <a href="sproduct.php?pid=<?= $row['id']?>"><img src="<?= $row['product_img'] ?>" width="100%"></a>
                     <div class="des">
                         <span><?= $row['product_brand'] ?></span>
                         <h5><?= $row['product_name'] ?></h5>
@@ -258,10 +258,6 @@
     </footer>
 
     <script src="https://kit.fontawesome.com/0164451027.js" crossorigin="anonymous"></script>
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
     <script src="script.js"></script>
 </body>
 
