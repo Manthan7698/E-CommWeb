0e1c0f08cf994afcbfb005512dfe5901d47b6c5a
HHet-Bhavsar <hhetbhavsar@gmail.com>
2025-03-26 13:58:53 +0530

    added

--- contact.php
+++ contact.php
@@ -94,59 +94,7 @@ session_start();
         </div>
     </section>
 
-    <footer class="section-p1">
-        <div class="col">
-            <img class="logo" src="img/logo.png" alt="" />
-            <h4>Contact</h4>
-            <p>
-                <strong>Address:</strong> 562 Wellington Road, Street 32, San
-                Francisco
-            </p>
-            <p><strong>Phone:</strong> (+91) 9510670132 / (+91) 8866830048</p>
-            <p><strong>Hours:</strong> 10:00 - 18:00, Mon - Sat</p>
-            <div class="follow">
-                <h4>Follow Us</h4>
-                <div class="icons">
-                    <a href="#" title="Facebook"><i class="fa-brands fa-facebook-f"></i></a>
-                    <a href="#" title="Twitter"><i class="fa-brands fa-x-twitter"></i></a>
-                    <a href="#" title="Instagram"><i class="fa-brands fa-instagram"></i></a>
-                    <a href="#" title="Pinterest"><i class="fa-brands fa-pinterest-p"></i></a>
-                    <a href="#" title="YouTube"><i class="fa-brands fa-youtube"></i></a>
-                </div>
-            </div>
-        </div>
-        <div class="col">
-            <h4>About</h4>
-            <a href="#">About us</a>
-            <a href="#">Delivery Information</a>
-            <a href="#">Privacy Policy</a>
-            <a href="#">Terms & Conditions</a>
-            <a href="#">Contact Us</a>
-        </div>
-
-        <div class="col">
-            <h4>My Account</h4>
-            <a href="#">Sing-in</a>
-            <a href="#">View Cart</a>
-            <a href="#">My Wishlist</a>
-            <a href="#">Track My Order</a>
-            <a href="#">Help</a>
-        </div>
-
-        <div class="col install">
-            <h4>Install App</h4>
-            <p>From App Store or Google Play Store</p>
-            <div class="row">
-                <img src="img/pay/app.jpg" alt="" />
-                <img src="img/pay/play.jpg" alt="" />
-            </div>
-            <p>Secured Payment Gateways</p>
-            <img src="img/pay/pay.png" alt="" />
-        </div>
-        <div class="copyright">
-            <p>© 2024, E-Commerce Website</p>
-        </div>
-    </footer>
+    <?php include 'footer.php' ?>
 
     <script src="https://kit.fontawesome.com/0164451027.js" crossorigin="anonymous"></script>
     <script src="script.js"></script>
